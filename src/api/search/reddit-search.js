4e2a43fbb4f3eb6e3659404a3ff8b49de3a189a1
cl4ireee <cl4ireeee@gmail.com>
2025-02-16 12:01:26 +0700

    Update reddit-search.js

--- src/api/search/reddit-search.js
+++ src/api/search/reddit-search.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const { default: axios } = require("axios");
 
 module.exports = (app) => {
     app.get("/search/reddit-search", async (req, res) => {
@@ -9,7 +9,11 @@ module.exports = (app) => {
         }
 
         try {
-            const { data } = await axios.get(`https://www.reddit.com/search.json?q=${q}`);
+            const { data } = await axios.get(`https://www.reddit.com/search.json?q=${q}`, {
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                }
+            });
             res.json({ status: true, result: data });
         } catch (error) {
             console.error("Error fetching Reddit search:", error.message);
