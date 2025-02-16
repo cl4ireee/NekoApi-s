1726bc43db506e58785b7ba175fd33ca431f5e4a
cl4ireee <cl4ireeee@gmail.com>
2025-02-16 14:14:55 +0700

    Update spotify.js

--- src/api/search/spotify.js
+++ src/api/search/spotify.js
@@ -8,13 +8,7 @@ const Spotify = {
     async details(url) {
         const response = await fetch(`https://spotifydown.app/api/metadata?link=${url}`, { method: "POST" });
         return response.json();
-    },
-    async download(url) {
-        const response = await fetch(`https://spotifydown.app/api/download?link=${url}`, {
-            headers: { Referer: "https://spotifydown.app/" },
-        });
-        return response.json();
-    },
+    }
 };
 
 // Ekspor fungsi untuk digunakan dengan `app.get`
@@ -50,23 +44,6 @@ module.exports = function (app) {
         } catch (error) {
             console.error("Error fetching Spotify details:", error.message);
             res.status(500).json({ status: false, error: "Failed to fetch Spotify details" });
-        }
-    });
-
-    // Route untuk mengunduh lagu
-    app.get("/download/spotify-download", async (req, res) => {
-        const { url } = req.query;
-
-        if (!url) {
-            return res.status(400).json({ status: false, error: "Query parameter (url) is required" });
-        }
-
-        try {
-            const results = await Spotify.download(url);
-            res.status(200).json({ status: true, results });
-        } catch (error) {
-            console.error("Error downloading from Spotify:", error.message);
-            res.status(500).json({ status: false, error: "Failed to download from Spotify" });
         }
     });
 };
