c01972ca01feb8f183143d98f9080c8eef19907a
cl4ireee <cl4ireeee@gmail.com>
2025-02-18 12:33:25 +0700

    Update kaori-nusantara.js

--- src/api/news/kaori-nusantara.js
+++ src/api/news/kaori-nusantara.js
@@ -2,15 +2,20 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 module.exports = function (app) {
+    // Endpoint untuk mengambil berita anime
     app.get('/news/anime-news', async (req, res) => {
         const url = 'https://www.kaorinusantara.or.id/rubrik/aktual/anime';
 
         try {
             const { data } = await axios.get(url, {
                 headers: {
-                    'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                    'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Connection': 'keep-alive'
                 }
             });
+
             const $ = cheerio.load(data);
             let news = [];
 
