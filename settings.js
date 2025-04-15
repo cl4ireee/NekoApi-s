1372e118eebe0bd13cfe1ea302a65b032d103a1c
cl4ireee <cl4ireeee@gmail.com>
2025-04-16 03:03:35 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: {
+        main: 'Hiuraa API Base !!',
+        copyright: 'Hiuraa API Base'
+    },
+    description: 'Integrated API solution for your modern application development needs. Fast, secure, and reliable access.',
+    icon: '/image/icon.png',
+    author: 'Rynn',
+    info_url: 'https://whatsapp.com/channel/0029Vb9MIdiDDmFXasVPVG1t',
+    links: [
+        {
+            name: 'WhatsApp Information Ch.',
+            url: 'https://whatsapp.com/channel/0029Vb9MIdiDDmFXasVPVG1t'
+        }
+    ]
+};
